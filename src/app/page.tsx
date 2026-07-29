b09b19a394715c96feca0173883cb800e36f0e64
Maria Julia Fantagussi <133687198+MajuFantagussi@users.noreply.github.com>
2026-07-29 14:47:36 -0300

    feat: Add FAQ sections (#6)
    * feat: Add Beneficios
    
    * feat: Add Beneficts
    
    * feat: Add FAQ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Benefits from "@/components/Benefits";
 import Clinics from "@/components/Clinics";
+import Faq from "@/components/Faq";
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
 
@@ -10,6 +11,8 @@ export default function Home() {
       <Services />
       <Benefits />
       <Clinics />
+      <Faq />
+
     </>
   );
 }
